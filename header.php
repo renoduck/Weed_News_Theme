ab56f1f97fac1a2533b572affb954cf4a6a7ec94
renoduck <renoduck@gmail.com>
2017-01-25 23:34:14 -0800

    Add files via upload

--- header.php
+++ header.php
@@ -23,7 +23,8 @@ do_action( 'suri_hook_before_html' ); ?>
 
 <html class="no-js" <?php language_attributes(); ?>>
 <head<?php suri_attr( 'head' );?>>
-<link rel="dns-prefetch" href="http://weedsnews.cdn.adglare.net/">
+<link rel="dns-prefetch" href="//weednews.cdn.adglare.net">
+<link rel="dns-prefetch" href="//weednews.engine.adglare.net">
 <?php wp_head(); ?>
 <meta name="theme-color" content="#2f9f46">
 <?php include("cstyle.php") ?>
